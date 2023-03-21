70da171d38c17a192baa4854126ea9818d54d2be
LEEHYUNDONG <l.steve10@yahoo.com>
2023-03-21 20:09:05 +0900

    [230321][이현동] intToRoman

--- HashTable/이현동/intToRoman.java
+++ HashTable/이현동/intToRoman.java
@@ -2,10 +2,10 @@ class Solution {
     public String intToRoman(int num) {
         // 1 <= num <= 3999
         String ans = "";
-        ArrayList<Integer> nums = new ArrayList<Integer>(Arrays.asList(1000,900, 500, 400, 100, 90 , 50, 40, 10, 9, 5, 4, 1));
+        int[] nums = Arrays.asList(1000,900, 500, 400, 100, 90 , 50, 40, 10, 9, 5, 4, 1);
         Map<Integer, String> roman = new HashMap<>();
-        roman.put( 1000, "M");
-        roman.put( 900, "CM" );
+        roman.put(1000, "M");
+        roman.put(900, "CM" );
         roman.put(500, "D" );
         roman.put(400, "CD" );
         roman.put(100, "C" );
